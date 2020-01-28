ba23d7db6e77a6b0fc388faaeca356b86962f9e5
平林宏淳 <hh@MacBook-Pro.local>
2020-01-29 01:29:32 +0900

    ログイン先設定

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+  def index
+
+  end
+
+  def new_guest
+    user = User.guest
+    sign_in user
+    redirect_to users_path, notice: 'ゲストユーザーとしてログインしました。'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
 
+  get 'users/index'
   root 'homes#index'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     passwords: 'users/passwords'
   }
   devise_scope :user do
-    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+    post 'users/guest_sign_in', to: 'users#new_guest'
+
+  resources :users
+
   end
 
 
